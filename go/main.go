4985d471ee691c49ec4ed7455cd94014056f4185
TharinduWijewardane <tharinduw@wso2.com>
2024-10-08 21:10:43 +0530

    Simulate error

--- go/main.go
+++ go/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -14,22 +16,54 @@ var inventory = map[string]int{
 	"123": 5,
 	"456": 0,
 	"789": 10,
+	"500": 5,
 }
 
 type InventoryResponse struct {
 	InStock bool `json:"in_stock"`
 }
 
+type RequestTracker struct {
+	requests []time.Time
+	mutex    sync.Mutex
+}
+
+var tracker = RequestTracker{
+	requests: make([]time.Time, 0),
+}
+
+func (rt *RequestTracker) AddRequest() {
+	rt.mutex.Lock()
+	defer rt.mutex.Unlock()
+
+	now := time.Now()
+	rt.requests = append(rt.requests, now)
+
+	// Remove requests older than 5 seconds
+	for len(rt.requests) > 0 && now.Sub(rt.requests[0]) > 5*time.Second {
+		rt.requests = rt.requests[1:]
+	}
+}
+
+func (rt *RequestTracker) CountRequests() int {
+	rt.mutex.Lock()
+	defer rt.mutex.Unlock()
+
+	return len(rt.requests)
+}
+
 func checkInventory(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Received request to check inventory")
+	requestCount := tracker.CountRequests()
+	tracker.AddRequest()
+	log.Printf("Received request to check inventory. Requests in last 5 seconds: %d", requestCount)
 
 	vars := mux.Vars(r)
 	productID := vars["productId"]
 	log.Printf("Checking inventory for product ID: %s", productID)
 
 	// Check if the product ID is "500" and return a 500 status code if true
-	if productID == "500" {
-		log.Printf("Product ID 500 requested, returning HTTP 500 status code")
+	if productID == "500" && requestCount == 0 {
+		log.Printf("Product ID 500 requested for the first time. Simiulating a 500 error")
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
