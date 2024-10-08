cc7db4ccc813acfc79d0ce604309b39029c3e167
TharinduWijewardane <tharinduw@wso2.com>
2024-10-08 19:22:03 +0530

    Add inventory service

--- go/go.mod
+++ go/go.mod
@@ -1,3 +1,5 @@
 module github.com/MrSupiri/choreo-byoc-examples/go
 
-go 1.18
+go 1.22
+
+require github.com/gorilla/mux v1.8.1
--- go/main.go
+++ go/main.go
@@ -3,84 +3,42 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	httpPort := 9090
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-
-		fmt.Fprintf(w, "{\"active\": true}")
-	})
-	http.HandleFunc("/healthz/", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-
-		fmt.Fprintf(w, "{\"healthy\": true}")
-	})
-	http.HandleFunc("/hello/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Hello %s", req.URL.Query().Get("name"))
-	})
-	http.HandleFunc("/five", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "{\"stauts\": 500}")
-	})
-	http.HandleFunc("/four09", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusConflict)
-		fmt.Fprintf(w, "{\"stauts\": 409}")
-	})
-	http.HandleFunc("/proxy/", func(w http.ResponseWriter, req *http.Request) {
-		if req.Method == http.MethodPost {
-			decoder := json.NewDecoder(req.Body)
-			var data map[string]string
-			err := decoder.Decode(&data)
-			if err != nil {
-				w.Write([]byte(err.Error()))
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-
-			host := data["host"]
-			args := data["args"]
-
-			if len(host) == 0 {
-				host = "http://postman-echo.com"
-			}
-			if len(args) == 0 {
-				args = "get?foo1=bar1&foo2=bar2"
-			}
-
-			resp, err := http.Get(fmt.Sprintf("%s/%s", strings.TrimRight(host, "/"), strings.TrimLeft(args, "/")))
-			if err != nil {
-				w.Write([]byte(err.Error()))
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				w.Write([]byte(err.Error()))
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-			w.Write(body)
-			return
-		}
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	})
-
-	fmt.Printf("listening on %v\n", httpPort)
-
-	err := http.ListenAndServe(fmt.Sprintf(":%d", httpPort), logRequest(http.DefaultServeMux))
-	if err != nil {
-		log.Fatal(err)
-	}
+// Simplified inventory (in a real scenario, this would be a database)
+var inventory = map[string]int{
+	"123": 5,
+	"456": 0,
+	"789": 10,
 }
 
-func logRequest(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-		handler.ServeHTTP(w, r)
-	})
+type InventoryResponse struct {
+	InStock bool `json:"in_stock"`
+}
+
+func checkInventory(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	productID := vars["productId"]
+
+	response := InventoryResponse{InStock: false}
+
+	if quantity, exists := inventory[productID]; exists && quantity > 0 {
+		response.InStock = true
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/check_inventory/{productId}", checkInventory).Methods("GET")
+
+	fmt.Println("Inventory Service is running on :9090")
+	log.Fatal(http.ListenAndServe(":9090", router))
 }
