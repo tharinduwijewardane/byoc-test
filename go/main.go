2d91b8167fd11262c9a4dec4c2bd12e37660275f
TharinduWijewardane <tharinduw@wso2.com>
2025-11-10 13:57:10 +0530

    Change nov10 2

--- go/main.go
+++ go/main.go
@@ -24,7 +24,6 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	log.Println("received SHOULD_CRASH value: " + envValue)
 
 	io.WriteString(w, "OOMKill Simulator\n")
-	io.WriteString(w, "Send a POST request to /trigger to simulate OOMKill\n")
 
 	if envValue == "YES" {
 		go os.Exit(1)
